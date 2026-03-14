50a15e3d200e708f3a269fb6217f838747c7cc7c
Amrit Reule <reuleamrit47@gmail.com>
2026-03-15 02:31:05 +0545

    dfdfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+// import of router
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+
 //component vanaya ko type of function jasle ni JSX garxa html jasta syntax
 function App() {
-let name = "amrit reule"
-let age = 44
-let address = "nepal"
+
 
   return (
-
-    <div>
-    <h1 style={{color:"red", background:"white"}}>This tag are used in react project  {name}</h1>
-    <h2>My age is: {age}</h2>
-    <h3>My address is: {address}</h3>
-    </div>
-   
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<h1>Hello, World!</h1>} />
+    <Route path="/about" element={<h1>About Us</h1>} />
+    <Route path="/contact" element={<h3> this is a contact page</h3>} />
+  </Routes>
+</BrowserRouter>
   )
 }
 
