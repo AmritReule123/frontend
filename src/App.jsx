a8ed651046d149023e873f27c3d873edfac54e76
Amrit Reule <reuleamrit47@gmail.com>
2026-03-16 20:14:27 +0545

    changes now

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ function App() {
 
 
   return (
-<BrowserRouter>
+ <BrowserRouter>
   <Routes>
     <Route path="/" element={<h1>Hello, World!</h1>} />
     <Route path="/about" element={<h1>About Us</h1>} />
     <Route path="/contact" element={<h3> this is a contact page</h3>} />
+    <Route path="/services" element={<h2> this is our services tag</h2>} />
   </Routes>
-</BrowserRouter>
+</BrowserRouter> 
+
+
+
+
+
   )
 }
 
