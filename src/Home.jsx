532b5a9e5950c562b168f5cfc47822b2d74f710f
Amrit Reule <reuleamrit47@gmail.com>
2026-03-18 20:42:36 +0545

    enter a day18

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,17 +7,39 @@ function Home(){
     //     console.log(meroNumber)
     // }
     // function decreaseMeroNumber(){
-    //     meroNumber = meroNumber - 1
-     
+    //     meroNumber = meroNumber - 1 
     // }
-    console.log(useState(10))
-   
+
+
+const [age, setAge] = useState(1000)
+
+
+    // console.log(useState(10))
+// let something = useState(100)
+// let myValue = something[0]
+// let myFunction = something[1]
+
+
+function increaseMyValue(){
+    setAge(age + 1)
+
+}
+
+function decreaseMyValue(){
+    setAge(age - 1)
+}
+
+
     return (
        <div>
-        {/* <h1> {meroNumber}</h1>
+        <h1> {age}</h1>
         <button onClick={increaseMeroNumber}>+</button>
-<button onClick={decreaseMeroNumber}>-</button> */}
-       <h1>Learning useState hook</h1>
+<button onClick={decreaseMeroNumber}>-</button>
+
+
+       {/* <h1>my value is : {myValue}</h1>
+       <button onClickCapture={increaseMyValue}>+</button>
+       <button onClick={decreaseMyValue}>-</button> */}
        </div> 
     )
 }
